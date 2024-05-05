d88c865a5576c34c10413cfd2844e9453030b508
GpapPeaky <giorgospapamat@gmail.com>
2024-05-05 19:40:27 +0300

    Refactor events

--- src/events.h
+++ src/events.h
@@ -1,10 +1,10 @@
-#ifndef EVENTS
-#define EVENTS
-
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_keyboard.h>
 #include "init_win.h"
 #include "definitions.aux"
+
+#ifndef EVENTS
+#define EVENTS
 
 extern SDL_Rect viewport; /* Will be initialised in main */
 
@@ -14,6 +14,17 @@ extern SDL_Rect viewport; /* Will be initialised in main */
  * @param quit Checks if the program has quit or not
 */
 void events_handling(bool& quit);
+
+/**
+ * @brief displays info when hovering a province
+*/
+event info_on_hover(void);
+
+/**
+ * @brief Highlights a province when clicked
+*/
+event highlight_on_click(void);
+/*TODO*/
 
 /**
  * @brief Zoom in/out event
