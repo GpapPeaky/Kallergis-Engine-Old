2b093a6bcde88c80ada0f9486cb5fc9eacf69578
GpapPeaky <giorgospapamat@gmail.com>
2024-08-22 02:09:44 +0300

    Comment Revision

--- src/economy.cpp
+++ src/economy.cpp
@@ -1,5 +1,6 @@
 #include "economy.hpp"
 
 float I(eco local_economy){
-    return static_cast<float>((((local_economy.development.admin * 0.02) + (local_economy.development.prod * 0.01)) * (local_economy.infrastructure * 0.01) + (GP(local_economy.local_goods) * 0.01)) * 15);
+    /*                           TAX                                                                                PRODUCTION                                                                            */
+    return static_cast<float>((((local_economy.development.admin * 0.02) * (local_economy.infrastructure * 0.01)) + (GP(local_economy.local_goods) * 0.01)  + (local_economy.development.prod * 0.01)) * 15);
 }
